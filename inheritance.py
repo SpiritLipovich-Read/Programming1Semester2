04ce25d35424f83eabaf6b495805fdaf5340afe2
Spirit L-R <breadmakesyoufat2000@gmail.com>
2016-05-18 13:56:36 -0600

    Inheritance

---
+++ inheritance.py
@@ -0,0 +1,19 @@
+class Character:
+    print("Time to design a character!")
+
+    def _init_(self, gender, age, name):
+        self.gender = g
+        self.age = a
+        self.name = n
+        
+def Hero(Character):
+    g = input ("Please pick either f, for female, m, for male, or o, for other")
+    a = input ("Please pick an age")
+    n = ("Please name your character")
+
+#def Villan (Character):
+    #g = "male"
+    #a = 18
+    #n = "Chase"
+
+    
